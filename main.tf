d8bd1409ddd8f559217e43622714e30992d4dc08
Levent <levent.eren@deop.ca>
2023-01-05 23:24:49 -0500

    first resource group

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-#  resource "azurerm_resource_group" "rg_kurs" {
-#    name     = var.rg_name
-#    location = var.location
-#  }
-
-#  resource "azurerm_storage_account" "storage_kurs" {
-#    name                     = "tfcloudstorage0099880"
-#    resource_group_name      = azurerm_resource_group.rg_kurs.name
-#    location                 = azurerm_resource_group.rg_kurs.location
-#    account_tier             = "Standard"
-#    account_replication_type = "LRS"
-
-#    tags = {
-#     environment = "staging"
-#   }
-#  }
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
